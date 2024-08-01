adc47df9e17a9c330a968ab6057282136ecdac41
Srihitha <srihitha0313@gmail.com>
2024-08-01 10:15:42 +0530

    New Branch

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import SignIn from "../SignInUp/SignIn";
 import SignUp from '../SignInUp/SignUp';
 import "./Header.css"
@@ -8,12 +8,13 @@ const Header = () => {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState('');
-const navigate = useNavigate()
+
+  const navigate = useNavigate();
   const handleSignIn = () => {
     setIsSignInOpen(true);
     setIsSignUpOpen(false);
     navigate("/signin");
-    
+
   };
 
   const handleSignUp = () => {
@@ -35,49 +36,49 @@ const navigate = useNavigate()
 
   return (
     <>
-    <div className='header-wrapper'>
-      <header>
-        <div className='header-wrapper-inside'>
-        <div className="logo">
-          <h4>ThePropertyForSale</h4>
-          {/* <img src="/path/to/logo.png" alt="Logo" /> */}
-        </div>
-        <nav className="navbar">
-          <ul className="nav-links">
-            <li><Link className="Header-Menu " to="/">Home</Link></li>
-            <li><Link className="Header-Menu " to="/about">About</Link></li>
-            <li className="dropdown">
-              <a className="Header-Menu ">Dropdown</a>
-              <div className="dropdown-content">
-                <Link className="dropdown-menu" to="/Images">Images</Link>
-                <Link className="dropdown-menu" to="/Products">Products</Link>
-              </div>
-            </li>
-          </ul>
-        </nav>
-        <div className="auth-buttons">
-     {isLoggedIn ? (
-       <>
-         <span className='name-display'>Hello, {username}</span>
-         <button onClick={handleSignOut}>Logout</button>
-       </>
-     ) : (
-       <>
-         <button onClick={handleSignIn}>Sign In</button>
-         <button onClick={handleSignUp}>Sign Up</button>
-          
-       </>
-     )}
-    
-   </div>
-        </div>
-      </header>
-      
-       </div>
-       {/* <Link className="dropdown-menu" to="/Products">Products</Link> */}
-   <Sig      nIn isOpen={isSignInOpen} onClose={() => setIsSignInOpen(false)} onLogin={handleLogin} />
-   <SignUp isOpen={isSignUpOpen} onClose={() => setIsSignUpOpen(false)} />
-   </>
+      <div className='header-wrapper'>
+        <header>
+          <div className='header-wrapper-inside'>
+            <div className="logo">
+              <h4>The Property For Sale</h4>
+              {/* <img src="/path/to/logo.png" alt="Logo" /> */}
+            </div>
+            <nav className="navbar">
+              <ul className="nav-links">
+                <li><Link className="Header-Menu " to="/">Home</Link></li>
+                <li><Link className="Header-Menu " to="/about">About</Link></li>
+                <li className="dropdown">
+                  <a className="Header-Menu ">Dropdown</a>
+                  <div className="dropdown-content">
+                    <Link className="dropdown-menu" to="/Images">Images</Link>
+                    <Link className="dropdown-menu" to="/Products">Products</Link>
+                  </div>
+                </li>
+              </ul>
+            </nav>
+            <div className="auth-buttons">
+              {isLoggedIn ? (
+                <>
+                  <span className='name-display'>Hello, {username}</span>
+                  <button onClick={handleSignOut}>Logout</button>
+                </>
+              ) : (
+                <>
+                  <button onClick={handleSignIn}>Sign In</button>
+                  <button onClick={handleSignUp}>Sign Up</button>
+
+                </>
+              )}
+
+            </div>
+          </div>
+        </header>
+
+      </div>
+      {/* <Link className="dropdown-menu" to="/Products">Products</Link> */}
+      <Sig nIn isOpen={isSignInOpen} onClose={() => setIsSignInOpen(false)} onLogin={handleLogin} />
+      <SignUp isOpen={isSignUpOpen} onClose={() => setIsSignUpOpen(false)} />
+    </>
   );
 }
 
